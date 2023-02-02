f0e31a538a99964584c3c2ccd13783c7e97ff75b
Andrew Leonard <andrewcleonard@fastmail.com>
2023-02-01 17:46:27 -0800

    WIP

--- src/App.js
+++ src/App.js
@@ -24,32 +24,6 @@ export default function Board(value, setValue) {
 		setSquares(nextSquares);
 	}
 
-import React, { useState } from "react";
-
-// const [value, setValue] = useState(null);
-// function handleClick() {
-// 	setValue("X");
-// }
-
-// maintains which squares are filled
-export default function Board(value, setValue) {
-	const [squares, setSquares] = useState(Array(9).fill(null)); // create an array with 9 elements and set each to `null`.
-
-	/*
-	 * reminder: inner `handleClick` function has access to outer `Board` function.
-	 * - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
-	 */
-
-	function handleClick(i) {
-		// `slice()` Array method creates a copy of `squares` array named `nextSquares`
-		const nextSquares = squares.slice();
-		console.log(`nextSquares = ${nextSquares}`);
-		// add `X` to the first (`[0]` index) square
-		nextSquares[i] = "X";
-		// tell React the state of the component has changed
-		setSquares(nextSquares);
-	}
-
 	return (
 		<React.Fragment>
 			<div className='board-row'>
@@ -59,6 +33,9 @@ export default function Board(value, setValue) {
 				<Square value={squares[2]} />
 			</div>
 			<div className='board-row'>
+				<Square value={squares[3]} />
+				<Square value={squares[4]} />
+				<Square value={squares[5]} />
 				<Square value={squares[3]} />
 				<Square value={squares[4]} />
 				<Square value={squares[5]} />
