f9616ac264f347c75162af17889dc8af41627a4f
Andrew Leonard <AndrewCLeonard@fastmail.com>
2023-02-01 22:01:25 -0800

    WIP: https://beta.reactjs.org/learn/tutorial-tic-tac-toe\#declaring-a-winner

--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 | Word      | Definition                                                                                                                                                                           |
 | --------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ |
 | component | <ul><li>a piece of reusable code that represents a part of a user interface</li><li>Components are used to render, manage, and update the UI elements in your application.</li></ul> |
+
+## Explanation
+
+Let’s recap what happens when a user clicks the top left square on your board to add an X to it:
+
+1. Clicking on the upper left square runs the function that the button received as its onClick prop from the Square. The Square component received that function as its onSquareClick prop from the Board. The Board component defined that function directly in the JSX. It calls handleClick with an argument of 0.
+2. handleClick uses the argument (0) to update the first element of the squares array from null to X.
+3. The squares state of the Board component was updated, so the Board and all of its children re-render. This causes the value prop of the Square component with index 0 to change from null to X.
+
+In the end the user sees that the upper left square has changed from empty to having a X after clicking it.
+
+## Conventions
+
+In React, it’s conventional to use `on[Event]` names for props which represent events and `handle[Event]` for the function definitions which handle the events.
+
+## Immutability
+
+-   ability to undo/redo is a common requirement for apps
+-   Avoiding direct data mutation lets you keep previous versions of data intact to reuse (or reset to them) later.
+-   By default, all child coemponents re-render automatically when state of a parent component changes.
+    - includes child components that weren't affected by change
+    - Try to avoid re-rendering a part of the tree that wasn't affected by it for faster performance. 
--- src/App.js
+++ src/App.js
@@ -6,64 +6,82 @@ import React, { useState } from "react";
 // }
 
 // maintains which squares are filled
-export default function Board(value, setValue) {
+export default function Board() {
+	const [xIsNext, setXIsNext] = useState(true);
 	const [squares, setSquares] = useState(Array(9).fill(null)); // create an array with 9 elements and set each to `null`.
 
 	/*
 	 * reminder: inner `handleClick` function has access to outer `Board` function.
 	 * - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
 	 */
-	
-	function handleClick() {
+
+	function handleClick(i) {
+		// check if square already has an "X" or "O"
+		if (squares[i]) {
+			return;
+		}
+
 		// `slice()` Array method creates a copy of `squares` array named `nextSquares`
 		const nextSquares = squares.slice();
-		console.log(`nextSquares = ${nextSquares}`);
-		// add `X` to the first (`[0]` index) square
-		nextSquares[0] = "X";
+		if (xIsNext) {
+			nextSquares[i] = "X";
+		} else {
+			nextSquares[i] = "O";
+		}
 		// tell React the state of the component has changed
 		setSquares(nextSquares);
+		// flip `xIsNext` boolean
+		setXIsNext(!xIsNext);
 	}
 
 	return (
 		<React.Fragment>
 			<div className='board-row'>
-				{/* `Board` component passes prop down to each of the `Square` components it renders */}
-				<Square value={squares[0]} onSquareClick={handleClick} />
-				<Square value={squares[1]} />
-				<Square value={squares[2]} />
+				{/* `Board` component maintains which squares are filled. It passes prop down to each of the `Square` components it renders */}
+				{/* coding `handleClick(0) would call the function, which calls `setSquares` and re-renders the board, which calls `handleClick(0)`, creating an infinite loop*/}
+				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+				<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
 			</div>
 			<div className='board-row'>
-				<Square value={squares[3]} />
-				<Square value={squares[4]} />
-				<Square value={squares[5]} />
-				<Square value={squares[3]} />
-				<Square value={squares[4]} />
-				<Square value={squares[5]} />
+				<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+				<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+				<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
 			</div>
 			<div className='board-row'>
-				<Square value={squares[6]} />
-				<Square value={squares[7]} />
-				<Square value={squares[8]} />
-				<Square value={squares[6]} />
-				<Square value={squares[7]} />
-				<Square value={squares[8]} />
+				<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+				<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+				<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
 			</div>
 		</React.Fragment>
 	);
 }
 
-function Square({ value, onSquareClick }) {
-	return (
-		<button className='square' onClick={onSquareClick}>
-			{value}
-		</button>
-	); // receive `value` prop from `Board` component
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
 
+// receive `value` prop (`X`, `O`, or `null`) from `Board` component. // Pass down `onSquareClick` fn from `Board` to `Square.`
 function Square({ value, onSquareClick }) {
 	return (
 		<button className='square' onClick={onSquareClick}>
-			{value}
+			{value} {/* receive `value` prop from `Board` component */}
 		</button>
-	); // receive `value` prop from `Board` component
+	);
 }
